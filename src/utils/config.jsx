a95ce206f73f9adfdf428ec6146b8d075aad7159
hariomp9 <hariompatil9@gmail.com>
2024-10-16 11:53:59 +0530

    update

--- src/utils/config.jsx
+++ src/utils/config.jsx
@@ -1,9 +1,4 @@
-// const config = {
-//     BASE_URL: "http://",
-//   };
-//   export default config;
 
 // export const Base_url = "https://instagram-clone-cyan-five.vercel.app";
-export const Base_url = "http://13.235.48.109:4000";
-
 // export const Base_url = "http://localhost:4000";
+export const Base_url = "http://3.110.28.248:4000";
