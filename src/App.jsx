b7b5fe28292de8b55611601191e724d1e703be63
hariomp9 <hariompatil9@gmail.com>
2024-10-16 17:06:50 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
 
       // listen all the events
       socketio.on("getOnlineUsers", (onlineUsers) => {
-        console.log(onlineUsers ,"onlineUsers")
+        // console.log(onlineUsers ,"onlineUsers")
         dispatch(setOnlineUsers(onlineUsers));
       });
 
